6106823aa70394a72303e13fa84081c07c698240
bagainu <bagayulan@163.com>
2018-04-13 19:39:30 +0800

    add crud rest api view for blog app

--- mysite/blog/api/urls.py
+++ mysite/blog/api/urls.py
@@ -1,11 +1,19 @@
 from django.urls import path
 from .views import (
     PostListAPIView,
+    PostCreateAPIView,
+    PostDetailAPIView,
+    PostUpdateAPIView,
+    PostDeleteAPIView,
 )
 
 
 app_name = 'api'
 urlpatterns = [
     path('', PostListAPIView.as_view(), name='blog_index'),
+    path('create/', PostCreateAPIView.as_view(), name='blog_create'),
+    path('b<int:blog_id>/', PostDetailAPIView.as_view(), name='blog_detail'),
+    path('b<int:blog_id>/update/', PostUpdateAPIView.as_view(), name='blog_update'),
+    path('b<int:blog_id>/delete/', PostDeleteAPIView.as_view(), name='blog_delete'),
 ]
 
--- mysite/blog/api/views.py
+++ mysite/blog/api/views.py
@@ -1,5 +1,11 @@
-from rest_framework.generics import ListAPIView
-
+from rest_framework.generics import (
+    ListAPIView, 
+    CreateAPIView,
+    RetrieveAPIView,
+    UpdateAPIView,
+    RetrieveUpdateAPIView,
+    DestroyAPIView,
+) 
 from blog.models import Post
 from .serializers import PostSerializer
 
@@ -7,3 +13,29 @@ from .serializers import PostSerializer
 class PostListAPIView(ListAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+
+class PostCreateAPIView(CreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class PostDetailAPIView(RetrieveAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    lookup_field = 'id'
+    lookup_url_kwarg = 'blog_id'
+
+
+class PostUpdateAPIView(RetrieveUpdateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    lookup_field = 'id'
+    lookup_url_kwarg = 'blog_id'
+
+
+class PostDeleteAPIView(DestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    lookup_field = 'id'
+    lookup_url_kwarg = 'blog_id'
